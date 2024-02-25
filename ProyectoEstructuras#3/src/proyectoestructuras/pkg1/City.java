acda23b928a6dd76d8fe8c856ac7a38a579585cc
LuisARRIETA2004 <122703126+LuisARRIETA2004@users.noreply.github.com>
2024-02-25 11:24:20 -0400

    Update City.java
    Comentarios Actualización de Ciudad

--- ProyectoEstructuras#3/src/proyectoestructuras/pkg1/City.java
+++ ProyectoEstructuras#3/src/proyectoestructuras/pkg1/City.java
@@ -9,40 +9,39 @@ desarroyo de clase y metodo city**/
 package proyectoestructuras.pkg1;
 
 public class City {
-    private Adyacentes paths;
-    private String name;
-    
-    public City(String name){
-        this.paths = new Adyacentes();
-        this.name = name;
-    }
+   private Adyacentes paths;  // Lista de caminos adyacentes a la ciudad
+   private String name;  // Nombre de la ciudad
+   
+   public City(String name){  // Constructor de la clase City que recibe un nombre como parámetro
+       this.paths = new Adyacentes();  // Inicializa la lista de caminos adyacentes como una nueva instancia de la clase Adyacentes
+       this.name = name;  // Asigna el nombre especificado a la ciudad
+   }
 
-    /**
-     * @return the paths
-     */
-    public Adyacentes getPaths() {
-        return paths;
-    }
+   /**
+    * @return the paths
+    */
+   public Adyacentes getPaths() {  // Método para obtener la lista de caminos adyacentes
+       return paths;
+   }
 
-    /**
-     * @param paths the paths to set
-     */
-    public void setPaths(Adyacentes paths) {
-        this.paths = paths;
-    }
+   /**
+    * @param paths the paths to set
+    */
+   public void setPaths(Adyacentes paths) {  // Método para establecer la lista de caminos adyacentes
+       this.paths = paths;
+   }
 
+   /**
+    * @return the name
+    */
+   public String getName() {  // Método para obtener el nombre de la ciudad
+       return name;
+   }
 
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * @param name the name to set
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
+   /**
+    * @param name the name to set
+    */
+   public void setName(String name) {  // Método para establecer el nombre de la ciudad
+       this.name = name;
+   }
 }
