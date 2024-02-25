90753f5a56ce98298171230c368db31add8ae2b8
LuisARRIETA2004 <122703126+LuisARRIETA2004@users.noreply.github.com>
2024-02-25 11:21:17 -0400

    Update Ant.java
    Comentarios Ant

--- ProyectoEstructuras#3/src/proyectoestructuras/pkg1/Ant.java
+++ ProyectoEstructuras#3/src/proyectoestructuras/pkg1/Ant.java
@@ -10,14 +10,14 @@ desarroyo de clase y metodo de los graphos**/
 package proyectoestructuras.pkg1;
 
 public class Ant {
-    double totalDistance;
-    City[] visitedCities;
+    double totalDistance;  // Almacena la distancia total recorrida por la hormiga
+    City[] visitedCities;  // Almacena las ciudades visitadas por la hormiga
     
-    public Ant(int size){
-        this.totalDistance = 0;
-        this.visitedCities = new City[size];
+    public Ant(int size){  // Constructor de la clase Ant que recibe el tamaño como parámetro
+        this.totalDistance = 0;  // Inicializa la distancia total en cero
+        this.visitedCities = new City[size];  // Inicializa el arreglo de ciudades visitadas con el tamaño especificado
         for (int i = 0; i < size; i++) {
-            visitedCities[i] = new City("");
+            visitedCities[i] = new City("");  // Inicializa cada elemento del arreglo como una nueva instancia de la clase City con un nombre vacío
         }
     }
 }
